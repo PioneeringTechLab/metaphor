b4ac7cb094e415b78c21af1e65157a7ef0f2971b
Alex Hernandez <oorangecchicken@gmail.com>
2016-04-25 18:48:49 -0700

    UI Kit Init

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,23 @@ var scssLint = require('laravel-elixir-scss-lint');
 var basePath = {
   bower: './bower_components',
   styles: {
-    src: '.src/scss',
+    src: './src/sass',
     dist: './dist/css'
   },
   scripts: {
-    src: '.src/js',
-    dist: './dist/js',
+    src: './src/js',
+    dist: './dist/js'
   },
   fonts: './dist/fonts'
 };
 
 
 elixir(function(mix) {
+  mix.scripts([
+    'scripts.js',
+  ], basePath.scripts.dist + '/metaphor.js', basePath.scripts.src );
+
   mix.sass('metaphor.scss', basePath.styles.dist, basePath.styles.src);
+
   mix.scssLint();
 });
