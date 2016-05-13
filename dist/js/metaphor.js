5c4ff568126e0c72428c07f30361bcd691d42f18
Alex Hernandez <oorangecchicken@gmail.com>
2016-05-12 18:59:48 -0700

    Made some awesome new changes, now its even awesomer

--- dist/js/metaphor.js
+++ dist/js/metaphor.js
@@ -7,11 +7,21 @@
     navLinks.toggleClass("primary-nav__links--open");
   });
 
+  var subNav = $(".sub-nav");
+  var subNavTop = subNav.offset().top;
   var subnavBtn = $(".sub-nav__btn");
   var subnavLinks = $(".sub-nav__links");
 
   subnavBtn.on("click", function(){
     subnavLinks.toggleClass("sub-nav__links--open");
+  });
+
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > subNavTop ) {
+      subNav.addClass("sub-nav--fixed");
+    } else {
+      subNav.removeClass("sub-nav--fixed");
+    }
   });
 
 })(jQuery);
